5d1d9d16e8bef48a54657b8ba24355c655ac1d73
flexpeak <59660805+flexpeak@users.noreply.github.com>
2022-08-11 12:33:32 -0400

    Update 20220528183103-create-pessoa.js

--- migrations/20220528183103-create-pessoa.js
+++ migrations/20220528183103-create-pessoa.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('pessoas', {
+    await queryInterface.createTable('Pessoas', {
       id: {
         allowNull: false,
         autoIncrement: true,
