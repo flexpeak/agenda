a0a3f25c374badc878605bcee0f41b934f09895b
flexpeak <59660805+flexpeak@users.noreply.github.com>
2022-08-11 12:33:44 -0400

    Update 20220604204000-create-telefone.js

--- migrations/20220604204000-create-telefone.js
+++ migrations/20220604204000-create-telefone.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('telefones', {
+    await queryInterface.createTable('Telefones', {
       id: {
         allowNull: false,
         autoIncrement: true,
